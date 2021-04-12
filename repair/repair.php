76161387731c3926dac6943c674636b951c644e0
Kelsey McCutcheon <kelsey.mccutcheon@mymail.champlain.edu>
2021-04-12 10:44:01 -0400

    moved files and added admin and repair

---
+++ repair/repair.php
@@ -0,0 +1,18 @@
+<?php
+//repair php functions
+//Kelsey McCutcheon, Griffin Baxter, Jacob Capra
+
+#user given part ID lowers quantity of the
+#given part by 1, 
+#also enter info like what product it was used on?
+#admin's removeInventory function?
+function useItem(){
+    pass
+}
+
+#report an issue with a part
+#send it somewhere?
+function reportIssue
+
+
+?>
