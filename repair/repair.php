a23698332eb035ddef02469f8860b2a8ac22c61e
Kelsey McCutcheon <kelsey.mccutcheon@mymail.champlain.edu>
2021-04-22 10:39:16 -0400

    function assignments

--- repair/repair.php
+++ repair/repair.php
@@ -2,17 +2,26 @@
 //repair php functions
 //Kelsey McCutcheon, Griffin Baxter, Jacob Capra
 
+require "../database/connection.php";
+
+$connection = db_connect();
+
 #user given part ID lowers quantity of the
 #given part by 1, 
 #also enter info like what product it was used on?
-#admin's removeInventory function?
+#admin's removeInventory function
+#KELSEY
 function useItem(){
     pass
 }
 
 #report an issue with a part
-#send it somewhere?
-function reportIssue
+#part ID and description of issue
+#send it somewhere - to admin or vendor?
+#GRIFFIN
+function reportIssue(){
+    
+}
 
 
 ?>
